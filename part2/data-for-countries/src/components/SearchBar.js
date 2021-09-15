40fad421d2d33cd41ee1195f8929dc6cc0f44046
leo <leo@xps-13-9360.lan>
2021-09-15 15:54:12 +0800

    2.13

--- part2/data-for-countries/src/components/SearchBar.js
+++ part2/data-for-countries/src/components/SearchBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+
 const SearchBar = ({ countries, showList, setShowList }) => {
   const [searchCountry, setSearchCountry] = useState('')
   const handleSearchCountryChange = (event) => {
@@ -15,6 +16,36 @@ const SearchBar = ({ countries, showList, setShowList }) => {
     setShowList(matchInLoop)
   }
   const DisplayZone = ({ list }) => {
+    const SpecifiedOne = ({country}) => {
+      return (
+        <div>
+          <h2>{country.name}</h2>
+          <p>capital {country.capital}</p>
+          <p>population {country.population}</p>
+          <div>
+            <h4>languages</h4>
+            <ul>
+              {country.languages.map(lang => {
+                return <li key={lang.iso639_1}>{lang.name}</li>
+              })}
+            </ul>
+            <img src={country.flag} alt="flag" width="10%" height="10%"></img>
+          </div>
+        </div>
+      )
+    }
+    const Hidden = ({country}) => {
+      const [isShow, setIsShow] = useState(false)
+      const toggle = () => {
+        isShow === true ? setIsShow(false) : setIsShow(true)
+      }
+      return (
+        <div>
+          {country.name} <button type="button" onClick={ toggle }>show</button>
+          {isShow === false ? null : <SpecifiedOne country={country}/>}
+        </div>
+      )
+    }
     if (list.length === 0) {
       // no item in list in the beginnnig
       return <div></div>
@@ -26,26 +57,13 @@ const SearchBar = ({ countries, showList, setShowList }) => {
     } else if (2 <= list.length && list.length <= 10) {
       // 2~10 show countries name only
       return <div>
-        {list.map(item =>
-          <div key={item.name}> {item.name} </div>
-        )}
+        {list.map(item => {
+          return <div key={item.name}>  <Hidden country={item} /> </div>
+        })}
       </div>
     } else {
-      // 1 match
-      return <div>
-        <h2>{list[0].name}</h2>
-        <p>capital {list[0].capital}</p>
-        <p>population {list[0].population}</p>
-        <div>
-          <h4>languages</h4>
-          <ul>
-            {list[0].languages.map(lang => {
-              return <li key={lang.iso639_1}>{lang.name}</li>
-            })}
-          </ul>
-          <img src={list[0].flag} alt="flag" width="10%" height="10%"></img>
-        </div>
-      </div>
+      // match
+      return <SpecifiedOne country={list[0]} />
     }
   }
   return (
