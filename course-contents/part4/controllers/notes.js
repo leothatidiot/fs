9280aa5a52f6b1ad66558e193dc27c98852f40ce
Leo <m.leo.cen@outlook.com>
2022-05-19 21:19:30 +0800

    refactor: edit /controllers/notes.js, using async/await in notesRouter

--- course-contents/part4/controllers/notes.js
+++ course-contents/part4/controllers/notes.js
@@ -1,10 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
@@ -28,11 +27,8 @@ notesRouter.post('/', (request, response, next) => {
     date: new Date()
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
