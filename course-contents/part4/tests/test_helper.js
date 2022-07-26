603c950b821d4e2a5bd492ab04ddc7cdf54da8cd
Leo <m.leo.cen@outlook.com>
2022-07-27 04:42:21 +0800

    feat: part4/b-last refactor tests

--- course-contents/part4/tests/test_helper.js
+++ course-contents/part4/tests/test_helper.js
@@ -14,7 +14,7 @@ const initialNotes = [
 ]
 
 const nonExistingId = async () => {
-  const note = new Note({ content: 'illremovethissoon', date: new Date()})
+  const note = new Note({ content: 'willremovethissoon', date: new Date()})
   await note.save()
   await note.remove()
   return note._id.toString()
